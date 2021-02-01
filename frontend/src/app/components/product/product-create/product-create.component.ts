f041a39535366fafc94d5e60720387a44cfd32cf
Ailton <junior.peron@hotmail.com>
2021-02-01 13:21:48 -0300

    erro em alterar produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -9,6 +10,11 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product:Product = {
+    name: '',
+    price: 0.00
+  }
+
   constructor(private productService: ProductService, 
     private router: Router) { }
 
@@ -17,7 +23,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('produto criado')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('produto criado')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
